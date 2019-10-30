334d10b36145f7fa209a71e66f62fd15b773eaf0
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-10-30 13:19:14 -0400

    implemented how to play

--- app/src/main/java/com/example/pockethockey/HowToPlay.java
+++ app/src/main/java/com/example/pockethockey/HowToPlay.java
@@ -2,7 +2,11 @@ package com.example.pockethockey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HowToPlay extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class HowToPlay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
+
+        ImageView imageView = findViewById(R.id.backButtonHTP);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button = findViewById(R.id.newGameHTP);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Game.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
