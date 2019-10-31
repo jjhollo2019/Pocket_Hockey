fbb9e556706a63375c4f57758df121115a4483ef
Zach Amend <zamend@clemson.edu>
2019-10-31 16:45:31 -0400

    added leaderboard pages

---
+++ HighscoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.pockethockey;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
