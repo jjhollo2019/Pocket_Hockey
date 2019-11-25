5118d76d6b3c760385e353077fa02dd088523ff2
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-11-25 09:43:45 -0500

    added Player.java

--- Game.java
+++ Game.java
@@ -1,4 +1,4 @@
-package com.example.pockethockey;
+package com.example.frameworktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -9,23 +9,23 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 public class Game extends AppCompatActivity implements GameOver.OpenSelectedListener, SensorEventListener {
-    private int strikes = 0;     // 3 strikes means GAME OVER
-    private static int level = 1;     // Start at Level 1
-    private static int highestLevel = 1;     // Start at Level 1, don't ever decrement
+    public static int strikes = 0;     // 3 strikes means GAME OVER
+    public static int level = 1;     // Start at Level 1
+    public static int highestLevel = 1;     // Start at Level 1, don't ever decrement
     private static boolean isGameOver;
     private static boolean highScoreSet;
-    private HighScoreDatabase database;
+    private static HighScoreDatabase database;
+    private static FragmentManager fragmentManager;
 
     // Accelerometer data
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    private final float[] rotationMatrix = new float[9];
-    public static float[] orientationAngles = new float[3];
-    private final float[] accelerometerReading = new float[3];
-    private final float[] magnetometerReading = new float[3];
+    public static float phoneAngle;
 
     // Jeremy's variables
     FrameLayout game;
@@ -33,14 +33,18 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GamePanel(this));     // TODO: Fix animation
-        game = new FrameLayout(this);
-        database = new HighScoreDatabase(getApplicationContext());
 
-        // Hide the status bar
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        AppConstants.gameActivityContext = this;
+
+        AppConstants.initialization(this.getApplicationContext());
+
+        setContentView(new GameView(this));
+        game = new FrameLayout(this);
+        database = new HighScoreDatabase(getApplicationContext());
 
         highScoreSet = false;
         isGameOver = false;
@@ -54,6 +58,7 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
         // TODO: Investigate NullPointerException
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override
@@ -88,9 +93,7 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
          * Y = sensorEvent.values[1] -- this is primarily what we're concerned with
          * Z = sensorEvent.values[2]
          */
-        orientationAngles[0] = sensorEvent.values[0];
-        orientationAngles[1] = sensorEvent.values[1];
-        orientationAngles[2] = sensorEvent.values[2];
+        phoneAngle = sensorEvent.values[1];
     }
 
     @Override
@@ -132,17 +135,17 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
     }
 
     // If player scores
-    private void onLevelPass(){
+    public static void onLevelPass(){
         // Pass data to the splash screen to display the next level
-        Intent goToSplash = new Intent(Game.this, Splash.class);
+        Intent goToSplash = new Intent(AppConstants.gameActivityContext, Splash.class);
         goToSplash.putExtra("level", level++);
         goToSplash.putExtra("strikes", strikes);
         goToSplash.putExtra("highestLevel", highestLevel++);
-        startActivity(goToSplash);
+        AppConstants.gameActivityContext.startActivity(goToSplash);
     }
 
     // If player doesn't score (misses shot or doesn't shoot at all)
-    private void onLevelFail(){
+    public static void onLevelFail(){
         strikes++;
         if (strikes == 3){
             onGameEnd();
@@ -151,25 +154,24 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
         if (level > 1) level--;
 
         // Send level and strikes to Splash.java
-        Intent goToSplash = new Intent(Game.this, Splash.class);
+        Intent goToSplash = new Intent(AppConstants.gameActivityContext, Splash.class);
         goToSplash.putExtra("level", level);
         goToSplash.putExtra("strikes", strikes);
         goToSplash.putExtra("highestLevel", highestLevel);
-        startActivity(goToSplash);
+        AppConstants.gameActivityContext.startActivity(goToSplash);
     }
 
-    private void onGameEnd(){
+    private static void onGameEnd(){
         isGameOver = true;
 
-        FragmentManager manager = getSupportFragmentManager();
         if (!highScoreSet){
             // If the player got a new highscore, open HighScoreEnding
-            if (database.checkHighScores(getApplicationContext(), highestLevel)){
+            if (database.checkHighScores(AppConstants.gameActivityContext, highestLevel)){
                 // Open the NewHighscoreDialog dialog
                 NewHighscoreDialog dialog = new NewHighscoreDialog();
                 // Don't allow players to click outside the dialog box (we don't want it to disappear)
                 dialog.setCancelable(false);
-                dialog.show(manager, "Game Over (New Highscore!)");
+                dialog.show(fragmentManager, "Game Over (New Highscore!)");
                 highScoreSet = true;
                 return;
             }
@@ -178,7 +180,7 @@ public class Game extends AppCompatActivity implements GameOver.OpenSelectedList
         GameOver dialog = new GameOver();
         // Don't allow players to click outside the dialog box (we don't want it to disappear)
         dialog.setCancelable(false);
-        dialog.show(manager, "Game Over (No Highscore)");
+        dialog.show(fragmentManager, "Game Over (No Highscore)");
     }
 
     public static int getEndingLevel(){
--- Player.java
+++ Player.java
@@ -1,64 +1,106 @@
-package com.example.pockethockey;
+package com.example.frameworktest;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
+/**
+ * This class handles the game logic for the game player
+ * @pre The gameThread and gameView must be instantiated before using this class
+ * @post The player methods are available for use
+ */
+public class Player {
 
-public class Player extends GameObject{
-    final private float RIGHT_EDGE = 723;
+    //player variables
+    private int playerX, playerY, currentFrame, velocity;
+    public static int maxFrame;
 
-    private Bitmap spritesheet;
-    private boolean isShooting;
-    private boolean playing;
-    private Animation animation = new Animation();
-    private long startTime;
-
-    public Player(Bitmap res, int w, int h, int numFrames){
-        x = GamePanel.WIDTH/2;
-        y = GamePanel.HEIGHT/2;
-        height = h;
-        width = w;
-
-        isShooting = false;
-        if(!isShooting){
-            numFrames = 2;
-        }
-
-        Bitmap[] image = new Bitmap[numFrames];
-        spritesheet = res;
-
-        for(int i = 0; i < image.length; i++){
-            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
-        }
-
-        animation.setFrames(image);
-        animation.setDelay(10);
-
-        startTime = System.nanoTime();
+    /**
+     * This function is the class constructor for player
+     * @pre gameThread != null && gameView != null
+     * @post player X and Y is set to screen center && other variables are initialized
+     */
+    public Player(){
+        playerX = AppConstants.SCREEN_WIDTH/2 - AppConstants.getBitmapBank().getPlayerWidth()/2;
+        playerY = AppConstants.SCREEN_HEIGHT/2 - AppConstants.getBitmapBank().getPlayerHeight()/2;
+        currentFrame = 0;
+        maxFrame = 1;
+        velocity = 0;
     }
 
-    public void setShooting(boolean b) {
-        isShooting = b;
-        Bitmap[] image = new Bitmap[4];
-        for(int i = 0; i < image.length; i++){
-            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
-        }
-        animation.setFrames(image);
-        animation.setDelay(10);
+    /**
+     * This function is the getter for velocity
+     * @return is the class velocity
+     * @pre velocity != null
+     * @post the value of velocity is not changed
+     */
+    public int getVelocity() {
+        return velocity;
     }
 
-    public void update(){
-        long elapsed = (System.nanoTime() - startTime)/1000000;
-
-        if(elapsed > 100){
-            startTime = System.nanoTime();
-        }
-        animation.update();
+    /**
+     * This function will set the class velocity
+     * @param velocity = the new velocity to be set
+     * @pre velocity != null
+     * @post this.velocity = velocity
+     */
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
     }
 
-    public void draw(Canvas canvas){
-        canvas.drawBitmap(animation.getImage(), x, y, null);
+    /**
+     * This function is the getter for the current frame
+     * @return is the class current frame
+     * @pre currentFrame != null
+     * @post the value of currentFrame is not changed
+     */
+    public int getCurrentFrame() {
+        return currentFrame;
     }
 
-    public boolean getPlaying(){return playing;}
-    public void setPlaying(boolean b){playing = b;}
+    /**
+     * This function will set the class currentFrame
+     * @param currentFrame = the new currentFrame to be set
+     * @pre currentFrame != null
+     * @post this.currentFrame = currentFrame
+     */
+    public void setCurrentFrame(int currentFrame) {
+        this.currentFrame = currentFrame;
+    }
+
+    /**
+     * This function is the getter for playerX
+     * @return is the class playerX
+     * @pre playerX != null
+     * @post the value of playerX is not changed
+     */
+    public int getX() {
+        return playerX;
+    }
+
+    /**
+     * This function is the getter for playerY
+     * @return is the class playerY
+     * @pre playerY != null
+     * @post the value of playerY is not changed
+     */
+    public int getY() {
+        return playerY;
+    }
+
+    /**
+     * This function will set the class playerX
+     * @param playerX = the new playerX to be set
+     * @pre playerX != null
+     * @post this.playerX = playerX
+     */
+    public void setPlayerX(int playerX) {
+        this.playerX = playerX;
+    }
+
+    /**
+     * This function will set the class playerX
+     * @param playerY = the new playerY to be set
+     * @pre playerY != null
+     * @post this.playerY = playerY
+     */
+    public void setPlayerY(int playerY) {
+        this.playerY = playerY;
+    }
 }
