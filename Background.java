a7209414196c6d0df4704a0d45d33fb2cb591dc0
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-11-24 21:43:07 -0500

    added Background.java

--- Background.java
+++ Background.java
@@ -1,37 +1,76 @@
-package com.example.pockethockey;
+package com.example.frameworktest;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-
+/**
+ * This class handles the parallax scrolling for the background
+ * @pre The gameThread && gameView must be intialized before using this class
+ * @post The background image is displayed
+ */
 public class Background {
 
-    private Bitmap image;
-    private int x;
-    private int y;
+    //class variables
+    private int backgroundImageX, backgroundImageY, backgroundImageVelocity;
 
-    public Background(Bitmap res){
-        image = res;
+    /**
+     * This function is the class constructor
+     * @pre The gameThread must be running before use
+     * @post backgroundImageX = 0 && backgroundImageY = 0 && backgroundImageVelocity = 3;
+     */
+    public Background(){
+        //init class variables
+        backgroundImageX = 0;
+        backgroundImageY = 0;
+        backgroundImageVelocity = 3;
     }
 
-    public void update(float x, float y){
-        if(x > 0){
-            this.x += 1.5f;
-        }
-        if(x < 0){
-            this.x -= 1.5f;
-        }
-        if(y > 0){
-            this.y += 1.5f;
-        }
-        if(y < 0){
-            this.y -= 1.5f;
-        } else {
-            this.x = (int) x;
-            this.y = (int) y;
-        }
+    /**
+     * This function returns the X value of the image
+     * @return backgroundImageX
+     * @pre The class constructor must be called first
+     * @post The value of backgroundImageX is not changed
+     */
+    public int getX() {
+        return backgroundImageX;
     }
 
-    public void draw(Canvas canvas){
-        canvas.drawBitmap(image, x, y, null);
+    /**
+     * This function returns the Y value of the image
+     * @return backgroundImageY
+     * @pre The class constructor must be called first
+     * @post The value of backgroundImageY is not changed
+     */
+    public int getY() {
+        return backgroundImageY;
     }
+
+    /**
+     * This function sets the value backgroundImageX
+     * @param backgroundImageX = the new value of backgroundImageX
+     * @pre The class constructor must be called first
+     * @post this.backgroundImageX = backgroundImageX
+     */
+    public void setX(int backgroundImageX) {
+        this.backgroundImageX = backgroundImageX;
+    }
+
+    /**
+     * This function sets the value backgroundImageY
+     * @param backgroundImageY = the new value of backgroundImageY
+     * @pre The class constructor must be called first
+     * @post this.backgroundImageY = backgroundImageY
+     */
+    public void setY(int backgroundImageY) {
+        this.backgroundImageY = backgroundImageY;
+    }
+
+    /**
+     * This function returns the velocity value of the image
+     * @return backgroundImageVelocity
+     * @pre The class constructor must be called first
+     * @post The value of backgroundImageVelocity is not changed
+     */
+    public int getVelocity() {
+        return backgroundImageVelocity;
+    }
+
+
 }
