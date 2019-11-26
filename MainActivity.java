e644eb3369abfdc9361197b7997a0b83b1a9f536
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-11-26 15:55:43 -0500

    added theme music to title

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,4 @@
-/* Jeremy Holloway (jjhollo@clemson.edu, C20581376)
- * Zachary Amend (zamend@clemson.edu, C16422178)
- * CPSC-4150-001
- * 12/2/2019
- * Pocket Hockey
- */
-
-package com.example.pockethockey;
+package com.example.planeshooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -14,22 +7,19 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     * @param savedInstanceState current state of the application
-     * @post inflates the activity layout
-     * @post sets listeners for each button in the layout
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hide the status bar
+        SoundBank soundBank = new SoundBank(getApplicationContext());
+        soundBank.playTheme();
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
-        // Set listeners for the navigation buttons
         TextView newGame = findViewById(R.id.newGameTP);
         newGame.setOnClickListener(new View.OnClickListener() {
             @Override
