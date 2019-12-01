02ff24f9d4ecc27076f28e9fbc35044430c693c7
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-12-01 14:01:42 -0500

    finalized GameOver.java

--- GameOver.java
+++ GameOver.java
@@ -2,10 +2,9 @@
  * Zachary Amend (zamend@clemson.edu, C16422178)
  * CPSC-4150-001
  * 12/2/2019
- * Pocket Hockey
+ * Flight Training
  */
-
-package com.example.pockethockey;
+package com.example.flight_training;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -13,41 +12,52 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.fragment.app.DialogFragment;
 
+/**
+ * This class handles the end of game events and extends the dialogFragment to interact with the user
+ * @pre This class must extend DialogFragment
+ * @post The class is able to handle end game checking via a Dialog Fragment
+ */
 public class GameOver extends DialogFragment {
+
+    /**
+     * This interface is for the listener
+     * @pre class must extend DialogFragment
+     * @post onButtonChosen is available for use
+     */
     public interface OpenSelectedListener {
-        /**
-         * @pre 0 < which < 4
-         * @param which id of button chosen from GameOver dialog box
-         * @post intents to the respective activity
-         */
         void onButtonChosen(int which);
     }
 
-    // Sends which button was selected to the hosting activity
-    private GameOver.OpenSelectedListener myListener;
+    //declare listener
+    private OpenSelectedListener myListener;
 
     /**
-     * @param savedInstanceState current state of the application
-     * @return the newly created dialog box
-     * @post sets outputs for each respective button available
+     * This is the onCreate function for the Dialog
+     * @param savedInstanceState = saved variables
+     * @return created dialog
      */
     @Override
     public AlertDialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getActivity())
+                //set dialog title
                 .setTitle(R.string.gameOverTitle)
+                //set dialog message
                 .setMessage("Your Score: " + Splash.getEndingLevel())
+                //set the onclick for the positive response
                 .setPositiveButton(R.string.playAgainDialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // User clicked New Game
                         myListener.onButtonChosen(id);
                     }
                 })
+                //set negative response onClick
                 .setNegativeButton(R.string.quitDialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // User clicked Quit
                         myListener.onButtonChosen(id);
                     }
                 })
+                //set neutral response for onClick
                 .setNeutralButton(R.string.leaderboardDialog, new DialogInterface.OnClickListener() {
                     @Override
                     // User clicked Leaderboard
@@ -55,16 +65,18 @@ public class GameOver extends DialogFragment {
                         myListener.onButtonChosen(id);
                     }
                 })
+                //final call to create fragment
                 .create();
     }
 
     /**
-     * @param context current context of the application
-     * @post attaches dialog with listener to host activity
+     * The function overrides the onAttach to allow for the custom listener
+     * @param context
      */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        //set listener
         myListener = (OpenSelectedListener) context;
     }
 }
